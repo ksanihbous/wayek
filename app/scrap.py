28e096cb575f1f02f6601b254099f125d3f475a3
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2020-06-14 10:19:28 +0700

    Update scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -1,7 +1,12 @@
-import requests, re, json , random
+import requests, re, json , random , urllib
 from re import match
 from bs4 import BeautifulSoup, SoupStrainer
 _session = requests.session()
+
+def getStr(string,start,end, index = 1):
+    str = string.split(start)
+    str = str[index].split(end)
+    return str[0]
 
 def img(query):
 	imagedata = []
@@ -13,10 +18,46 @@ def img(query):
 			imagedata.append(clu['src'])
 	result = {
 		'status':'200',
-                'creator':'Asa Xyz',
+        'creator':'Asa Xyz',
 		'result':imagedata
 		}
 	return(result)
+
+def bmkg():
+    dsa ="https://www.bmkg.go.id/"
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'id,en-US;q=0.7,en;qe=0.3',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+    'TE': 'Trailers',
+    }
+    re = requests.get(dsa, headers=headers).text
+    waktu = getStr(re, '<li><span class="waktu">', '</li>')
+    magnitude = getStr(re, '<li><span class="ic magnitude"></span>', '</li>')
+    kedalaman = getStr(re, '<li><span class="ic kedalaman"></span>', '</li>')
+    kordinat = getStr(re, '<li><span class="ic koordinat"></span>', '</li>')
+    lokasi = getStr(re, '<li><span class="ic lokasi"></span>', '</li>')
+    dirasakan = getStr(re, '<li><span class="ic dirasakan"></span>', '</li>')
+    img = getStr(re, '<img class="img-responsive" src="', '"')
+    himbau = ["Hati-hati terhadap gempabumi yang akan terjadi selanjutnya","Buat warga sekitar di mohon untuk tetap waspada","Waspada gempabumi susulan yang mungkin saja terjadi","Warga sekitar di mohon untuk tidak panik dan tetap memantau kondisi yang akan mungkin terjadi"]
+    himbauan = random.choice(himbau)
+    result = {
+        "result": {
+            "lokasi": lokasi,
+            "waktu": waktu,
+            "magnitude": magnitude,
+            "kordinat": kordinat,
+            "kedalaman": kedalaman,
+            "subjek": "Gempabumi {}".format(dirasakan),
+            "img": img,
+            "himbauan": himbauan
+        }
+    }
+    return(result)
 
 def cctv(code):
     with requests.session() as s:
