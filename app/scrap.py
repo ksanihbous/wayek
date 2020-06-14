a40f8bf41d246cab733f948ef7174345ccfb1dc6
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2020-06-14 12:08:15 +0700

    Update scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -9,19 +9,19 @@ def getStr(string,start,end, index = 1):
     return str[0]
 
 def img(query):
-	imagedata = []
-	link = 'https://www.google.co.in/search?q={}&source=lnms&tbm=isch'.format(query)
-	soup = requests.get(link)
-	soup = BeautifulSoup(soup.content,"lxml")
-	for clu in soup.findAll('img'):
-		if 'textinput' not in clu['src']:
-			imagedata.append(clu['src'])
-	result = {
-		'status':'200',
+    imagedata = []
+    link = 'https://www.google.co.in/search?q={}&source=lnms&tbm=isch'.format(query)
+    soup = requests.get(link)
+    soup = BeautifulSoup(soup.content,"lxml")
+    for clu in soup.findAll('img'):
+        if 'textinput' not in clu['src']:
+            imagedata.append(clu['src'])
+    result = {
+        'status':'200',
         'creator':'Asa Xyz',
-		'result':imagedata
-		}
-	return(result)
+        'result':imagedata
+        }
+    return(result)
 
 def bmkg():
     dsa ="https://www.bmkg.go.id/"
@@ -66,13 +66,15 @@ def cctv(code):
     }
     uye = "http://lewatmana.com/cam/{}/bundaran-hi/".format(str(code))
     r = requests.get(uye, headers=headerss)
-    soup = BeautifulSoup(r.content, 'html5lib')
+    soup = BeautifulSoup(r.text, 'html5lib')
     soup2 = BeautifulSoup(r.content, 'lxml', parse_only=SoupStrainer('div', {'class':'panel-body'}))
     title = soup2.find('h1').get_text()
     location = soup2.find_all('p')[0].find_all('a')[1].get_text()
     video = soup.find('source')['src']
     desc = soup2.find_all('p')[1].get_text()
     result = {
+        "status":"200",
+        "creator":"Asa Xyz",
         "result": {
             "title": title,
             "description": desc,
@@ -100,6 +102,8 @@ def sifatnama(name):
     stia = ["0%","1%","2%","3%","4%","5%","6%","9%","10%","11%","12%","13%","14%","15%","16%","17%","18%","19%","20%","21%","22%","23%","24%","25%","26%","27%","28%","29%","30%","31%","32%","33%","34%","35%","36%","37%","38%","39%","40%","41%","42%","43%","44%","45%","46%","47%","48%","49%","50%","51","52","53","54%","55%","56%","57%","58%","59%","60%","61%","62%","63%","64%","65%","66%","67%","68%","69%","70%","71%","72%","73%","74%","75%","76%","77%","78%","79%","80%","81%","82%","83%","84%","85%","86%","87%","88%","89%","90%","91%","92%","93%","94%","95%","96%","97%","98%","99%","100%"]
     setia = random.choice(stia)
     result = {
+        "status":"200",
+        "creator":"Asa Xyz",
         "result": {
             "romantis": romantis,
             "ngangenin": ngangenin,
@@ -122,24 +126,24 @@ def instaprofile(user):
         getJsonInstagram = re.search(r'window._sharedData\s*=\s*(\{.+\})\s*;', getInfoInstagram).group(1)
         data = json.loads(getJsonInstagram)
         for instagramProfile in data["entry_data"]["ProfilePage"]:
-    	    username = instagramProfile["graphql"]["user"]["username"]
-    	    name = instagramProfile["graphql"]["user"]["full_name"]
-    	    picture = instagramProfile["graphql"]["user"]["profile_pic_url_hd"]
-    	    biography = instagramProfile["graphql"]["user"]["biography"]
-    	    followers = instagramProfile["graphql"]["user"]["edge_followed_by"]["count"]
-    	    following = instagramProfile["graphql"]["user"]["edge_follow"]["count"]
-    	    private = instagramProfile["graphql"]["user"]["is_private"]
-    	    media = instagramProfile["graphql"]["user"]["edge_owner_to_timeline_media"]["count"]
-    	    result = {
-    			"result": {
-    				"username": username,
-    				"fullname": name,
-    				"bio": biography,
-    				"followers": followers,
-    				"following": following,
-    				"media": media,
-    				"private": private,
-    				"profile_img": picture
-    			}
-    		}
-    	    return(result)
+            username = instagramProfile["graphql"]["user"]["username"]
+            name = instagramProfile["graphql"]["user"]["full_name"]
+            picture = instagramProfile["graphql"]["user"]["profile_pic_url_hd"]
+            biography = instagramProfile["graphql"]["user"]["biography"]
+            followers = instagramProfile["graphql"]["user"]["edge_followed_by"]["count"]
+            following = instagramProfile["graphql"]["user"]["edge_follow"]["count"]
+            private = instagramProfile["graphql"]["user"]["is_private"]
+            media = instagramProfile["graphql"]["user"]["edge_owner_to_timeline_media"]["count"]
+            result = {
+                "result": {
+                    "username": username,
+                    "fullname": name,
+                    "bio": biography,
+                    "followers": followers,
+                    "following": following,
+                    "media": media,
+                    "private": private,
+                    "profile_img": picture
+                }
+            }
+            return(result)
