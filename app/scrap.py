d7279f1344a0a516d7b587fdf8183147c475d71f
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2020-06-14 11:25:22 +0700

    Update scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -18,7 +18,7 @@ def img(query):
 			imagedata.append(clu['src'])
 	result = {
 		'status':'200',
-                'creator':'Asa Xyz',
+        'creator':'Asa Xyz',
 		'result':imagedata
 		}
 	return(result)
@@ -60,24 +60,26 @@ def bmkg():
     return(result)
 
 def cctv(code):
-    with requests.session() as s:
-        s.headers['user-agent'] = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-        r = s.get("http://lewatmana.com/cam/{}/bundaran-hi/".format(str(code)))
-        soup = BeautifulSoup(r.content, 'html5lib')
-        soup2 = BeautifulSoup(r.content, 'lxml', parse_only=SoupStrainer('div', {'class':'panel-body'}))
-        title = soup2.find('h1').get_text()
-        location = soup2.find_all('p')[0].find_all('a')[1].get_text()
-        video = soup.find('source')['src']
-        desc = soup2.find_all('p')[1].get_text()
-        result = {
-            "result": {
-                "title": title,
-                "description": desc,
-                "link_video": video,
-                "location": location
-            }
+    headerss = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    }
+    uye = "http://lewatmana.com/cam/{}/bundaran-hi/".format(str(code))
+    r = requests.get(uye, headers=headerss)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    soup2 = BeautifulSoup(r.content, 'lxml', parse_only=SoupStrainer('div', {'class':'panel-body'}))
+    title = soup2.find('h1').get_text()
+    location = soup2.find_all('p')[0].find_all('a')[1].get_text()
+    video = soup.find('source')['src']
+    desc = soup2.find_all('p')[1].get_text()
+    result = {
+        "result": {
+            "title": title,
+            "description": desc,
+            "link_video": video,
+            "location": location
         }
-        return(result)
+    }
+    return(result)
 
 def sifatnama(name):
     romnts = ["0%","1%","2%","3%","4%","5%","6%","9%","10%","11%","12%","13%","14%","15%","16%","17%","18%","19%","20%","21%","22%","23%","24%","25%","26%","27%","28%","29%","30%","31%","32%","33%","34%","35%","36%","37%","38%","39%","40%","41%","42%","43%","44%","45%","46%","47%","48%","49%","50%","51","52","53","54%","55%","56%","57%","58%","59%","60%","61%","62%","63%","64%","65%","66%","67%","68%","69%","70%","71%","72%","73%","74%","75%","76%","77%","78%","79%","80%","81%","82%","83%","84%","85%","86%","87%","88%","89%","90%","91%","92%","93%","94%","95%","96%","97%","98%","99%","100%"]
