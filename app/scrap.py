9b8922db57096ed85fde0ce8789a0a180cd6882b
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2020-06-14 11:14:38 +0700

    Update scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -18,7 +18,7 @@ def img(query):
 			imagedata.append(clu['src'])
 	result = {
 		'status':'200',
-        'creator':'Asa Xyz',
+                'creator':'Asa Xyz',
 		'result':imagedata
 		}
 	return(result)
@@ -46,8 +46,6 @@ def bmkg():
     himbau = ["Hati-hati terhadap gempabumi yang akan terjadi selanjutnya","Buat warga sekitar di mohon untuk tetap waspada","Waspada gempabumi susulan yang mungkin saja terjadi","Warga sekitar di mohon untuk tidak panik dan tetap memantau kondisi yang akan mungkin terjadi"]
     himbauan = random.choice(himbau)
     result = {
-        "status":"200",
-        "creator":"Asa Xyz",
         "result": {
             "lokasi": lokasi,
             "waktu": waktu,
@@ -99,8 +97,6 @@ def sifatnama(name):
     stia = ["0%","1%","2%","3%","4%","5%","6%","9%","10%","11%","12%","13%","14%","15%","16%","17%","18%","19%","20%","21%","22%","23%","24%","25%","26%","27%","28%","29%","30%","31%","32%","33%","34%","35%","36%","37%","38%","39%","40%","41%","42%","43%","44%","45%","46%","47%","48%","49%","50%","51","52","53","54%","55%","56%","57%","58%","59%","60%","61%","62%","63%","64%","65%","66%","67%","68%","69%","70%","71%","72%","73%","74%","75%","76%","77%","78%","79%","80%","81%","82%","83%","84%","85%","86%","87%","88%","89%","90%","91%","92%","93%","94%","95%","96%","97%","98%","99%","100%"]
     setia = random.choice(stia)
     result = {
-        "status":"200",
-        "creator":"Asa Xyz",
         "result": {
             "romantis": romantis,
             "ngangenin": ngangenin,
