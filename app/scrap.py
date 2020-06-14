6ea2b8694d5b0db02dfc6fa9e959678e3a22cc3f
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2020-06-14 12:26:26 +0700

    Update scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -145,32 +145,27 @@ def sifatnama(name):
     return(result)
 
 def instaprofile(user):
-    uReq = requests
-    bSoup = BeautifulSoup
-    website = uReq.get("https://www.instagram.com/{}/".format(str(user)))
-    data = bSoup(website.content, "lxml")
-    for getInfoInstagram in data.findAll("script", {"type":"text/javascript"})[3]:
-        getJsonInstagram = re.search(r'window._sharedData\s*=\s*(\{.+\})\s*;', getInfoInstagram).group(1)
-        data = json.loads(getJsonInstagram)
-        for instagramProfile in data["entry_data"]["ProfilePage"]:
-            username = instagramProfile["graphql"]["user"]["username"]
-            name = instagramProfile["graphql"]["user"]["full_name"]
-            picture = instagramProfile["graphql"]["user"]["profile_pic_url_hd"]
-            biography = instagramProfile["graphql"]["user"]["biography"]
-            followers = instagramProfile["graphql"]["user"]["edge_followed_by"]["count"]
-            following = instagramProfile["graphql"]["user"]["edge_follow"]["count"]
-            private = instagramProfile["graphql"]["user"]["is_private"]
-            media = instagramProfile["graphql"]["user"]["edge_owner_to_timeline_media"]["count"]
-            result = {
-                "result": {
-                    "username": username,
-                    "fullname": name,
-                    "bio": biography,
-                    "followers": followers,
-                    "following": following,
-                    "media": media,
-                    "private": private,
-                    "profile_img": picture
-                }
-            }
-            return(result)
+    r = requests.get("http://apitrojans.herokuapp.com/instagram/user?username={}".format(str(user)))
+    data=r.text
+    data=json.loads(data)
+    username = data["result"]["username"]
+    name = data["result"]["full_name"]
+    picture = data["result"]["profile_img"]
+    biography = data["result"]["bio"]
+    followers = data["result"]["followers"]
+    following = data["result"]["following"]
+    private = data["result"]["private"]
+    media = data["result"]["media"]
+    result = {
+        "result": {
+            "username": username,
+            "fullname": name,
+            "profile_img": picture,
+            "biography": biography,
+            "followers": followers,
+            "following": following,
+            "media": media,
+            "private": private
+        }
+    }
+    return(result)
