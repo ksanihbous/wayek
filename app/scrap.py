7e07b5e5d6b165e275cb7f2a85d4debeea499f4a
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2020-06-20 22:09:40 +0700

    Update scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -227,32 +227,35 @@ def bitly(link):
 
 def bitly2(link):
     try:
-        r = requests.get("https://api-ssl.bitly.com/v3/clicks?access_token=c52a3ad85f0eeafbb55e680d0fb926a5c4cab823&shortUrl={}".format(link))
-        data=r.text
-        data=json.loads(data)
-        url = data["data"]["clicks"][0]["short_url"]
-        has = data["data"]["clicks"][0]["user_hash"]
-        clickk = data["data"]["clicks"][0]["user_clicks"]
-        r = requests.get("https://api-ssl.bitly.com/v3/info?access_token=c52a3ad85f0eeafbb55e680d0fb926a5c4cab823&shortUrl={}".format(url))
-        data=r.text
-        data=json.loads(data)
-        title = data["data"]["info"][0]["title"]
-        result = {
-            "status":"200",
-            "creator":"Asa Xyz",
-            "result": {
-                "url_title": title,
-                "url_short": url,
-                "url_hash": has,
-                "url_click": clickk
+        if "http://bit.ly/" in link:
+            r = requests.get("https://api-ssl.bitly.com/v3/clicks?access_token=c52a3ad85f0eeafbb55e680d0fb926a5c4cab823&shortUrl={}".format(click))
+            data=r.text
+            data=json.loads(data)
+            url = data["data"]["clicks"][0]["short_url"]
+            has = data["data"]["clicks"][0]["user_hash"]
+            clickk = data["data"]["clicks"][0]["user_clicks"]
+            result = {
+                "status":"200",
+                "creator":"Asa Xyz",
+                "result": {
+                    "url_short": url,
+                    "url_hash": has,
+                    "url_click": clickk
+                }
             }
-        }
-        return(result)
+            return(result)
+        else:
+            result = {
+                "status":"403",
+                "creator":"Asa Xyz",
+                "message":"Forbidden URL Use http://bit.ly/",
+            }
+            return(result)
     except:
         result = {
             "status":"404",
             "creator":"Asa Xyz",
-            "message":"FORBIDDEN URL Use http://bit.ly/",
+            "message":"URL Not Found",
         }
         return(result)
 
