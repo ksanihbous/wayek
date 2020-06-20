854a76fb07ab15eef41465d71e2321ca6991cb67
ksanihbous <32399094+ksanihbous@users.noreply.github.com>
2020-06-20 22:20:14 +0700

    Update scrap.py

--- app/scrap.py
+++ app/scrap.py
@@ -234,10 +234,15 @@ def bitly2(link):
             url = data["data"]["clicks"][0]["short_url"]
             has = data["data"]["clicks"][0]["user_hash"]
             clickk = data["data"]["clicks"][0]["user_clicks"]
+            r = requests.get("https://api-ssl.bitly.com/v3/info?access_token=c52a3ad85f0eeafbb55e680d0fb926a5c4cab823&shortUrl={}".format(url))
+            data=r.text
+            data=json.loads(data)
+            title = data["data"]["info"][0]["title"]
             result = {
                 "status":"200",
                 "creator":"Asa Xyz",
                 "result": {
+                    "url_title": title,
                     "url_short": url,
                     "url_hash": has,
                     "url_click": clickk
